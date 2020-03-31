1a2fb36b34f75189328603bc738bde5b54b8125f
Yen-Chen Lin <yenchenlin1994@gmail.com>
2020-03-31 08:08:48 +0800

    Port load blender to torch; replacing tf.image.resize_area makes it super slow so not sure what to do

--- test_torch.py
+++ test_torch.py
@@ -27,11 +27,47 @@ def test_positional_encoding():
     assert np.allclose(y_torch, y_tf), "Positional encoding is wrong!"
 
 
+def test_hierarchical_sampling():
+    from create_nerf_torch import sample_pdf as sample_pdf_torch
+    from run_nerf_helpers import sample_pdf as sample_pdf_tf
+
+    bins = np.array([0., 1., 2., 4.])
+    weights = np.array([1.0, 1.0, 1.0])
+    N_samples = 3
+
+    print(sample_pdf_tf(bins, weights, N_samples))
+
+
+"""
 def test_model_architecture():
     from create_nerf_torch import init_nerf_model as init_nerf_model_torch
     from run_nerf_helpers import init_nerf_model as init_nerf_model_tf
+"""
 
 
+def test_load_blender_data():
+    from load_blender_torch import load_blender_data as load_blender_data_torch
+    from load_blender import load_blender_data as load_blender_data_tf
 
-    # TODO: add assertion
-    # assert ???
+    datadir = './test_data/nerf_synthetic/lego'
+    half_res = True
+    testskip = 100
+    white_bkgd = True
+
+    images_torch, poses_torch, render_poses_torch, hwf_torch, i_split_torch = load_blender_data_torch(datadir, half_res, testskip)
+    if white_bkgd:
+        images_torch = images_torch[...,:3]*images_torch[...,-1:] + (1.-images_torch[...,-1:])
+    else:
+        images_torch = images_torch[...,:3]
+
+    images_tf, poses_tf, render_poses_tf, hwf_tf, i_split_tf = load_blender_data_tf(datadir, half_res, testskip)
+    if white_bkgd:
+        images_tf = images_tf[...,:3]*images_tf[...,-1:] + (1.-images_tf[...,-1:])
+    else:
+        images_tf = images_tf[...,:3]
+
+    assert np.allclose(images_torch, images_tf)
+    assert np.allclose(poses_torch, poses_tf)
+    assert np.allclose(render_poses_torch.numpy(), render_poses_tf.numpy())
+
+# test_hierarchical_sampling()
