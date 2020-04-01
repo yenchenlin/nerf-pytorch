53234eeaa43418a3ee84e1b3dd3c889faaddc027
Yen-Chen Lin <yenchenlin1994@gmail.com>
2020-04-01 08:11:45 -0400

    Add test_get_rays

--- test_torch.py
+++ test_torch.py
@@ -26,6 +26,24 @@ def test_positional_encoding():
     
     assert np.allclose(y_torch, y_tf), "Positional encoding is wrong!"
 
+
+def test_get_rays():
+    from run_nerf_helpers_torch import get_rays, get_rays_np
+    H, W, focal = int(400 / 40), int(400 / 40), 555.555 / 40
+    hwf = [H, W, focal]
+    pose = np.array([
+        [-0.9305, 0.1170, -0.3469, -1.3986],
+        [-0.3661, -0.2975, 0.8817, 3.554],
+        [0, 0.9475, 0.3197, 1.288]
+    ])
+
+    rays_o_np, rays_d_np = get_rays_np(H, W, focal, pose)
+    rays_o_torch, rays_d_torch = get_rays(H, W, focal, torch.Tensor(pose))
+
+    assert np.allclose(rays_o_np, rays_o_torch.numpy())
+    assert np.allclose(rays_d_np, rays_d_torch.numpy())
+
+
 """
 def test_hierarchical_sampling():
     from run_nerf_helpers_torch import sample_pdf as sample_pdf_torch
@@ -51,7 +69,7 @@ def test_load_blender_data():
 
     datadir = './test_data/nerf_synthetic/lego'
     half_res = True
-    testskip = 100
+    testskip = 1
     white_bkgd = True
 
     images_torch, poses_torch, render_poses_torch, hwf_torch, i_split_torch = load_blender_data_torch(datadir, half_res, testskip)
@@ -69,5 +87,3 @@ def test_load_blender_data():
     assert np.allclose(images_torch, images_tf)
     assert np.allclose(poses_torch, poses_tf)
     assert np.allclose(render_poses_torch.numpy(), render_poses_tf.numpy())
-
-
