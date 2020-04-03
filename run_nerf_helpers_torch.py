4268da6124c70a2e5b9d946376972729fc04a3ef
Yen-Chen Lin <yenchenlin1994@gmail.com>
2020-04-02 21:58:12 -0400

    Fix torch meshgrid bug

--- run_nerf_helpers_torch.py
+++ run_nerf_helpers_torch.py
@@ -154,7 +154,9 @@ class NeRF(nn.Module):
 
 # Ray helpers
 def get_rays(H, W, focal, c2w):
-    j, i = torch.meshgrid(torch.linspace(0, W-1, W), torch.linspace(0, H-1, H))  # pytorch's meshgrid has indexing='ij'
+    i, j = torch.meshgrid(torch.linspace(0, W-1, W), torch.linspace(0, H-1, H))  # pytorch's meshgrid has indexing='ij'
+    i = i.t()
+    j = j.t()
     dirs = torch.stack([(i-W*.5)/focal, -(j-H*.5)/focal, -torch.ones_like(i)], -1)
     # Rotate ray directions from camera frame to the world frame
     rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
