a1e1d27156e84b20c05fdaf034f0587929b1dda6
Yen-Chen Lin <yenchenl@mit.edu>
2021-06-07 14:36:41 -0400

    Update load_blender.py

--- load_blender.py
+++ load_blender.py
@@ -81,7 +81,7 @@ def load_blender_data(basedir, half_res=False, testskip=1):
 
         imgs_half_res = np.zeros((imgs.shape[0], H, W, 4))
         for i, img in enumerate(imgs):
-            imgs_half_res[i] = cv2.resize(img, (H, W), interpolation=cv2.INTER_AREA)
+            imgs_half_res[i] = cv2.resize(img, (W, H), interpolation=cv2.INTER_AREA)
         imgs = imgs_half_res
         # imgs = tf.image.resize_area(imgs, [400, 400]).numpy()
 
