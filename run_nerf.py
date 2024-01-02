259b6f1dac7dce61829521c50cd07a139baa12c5
Mateusz Rajski <mateusz.rajski@swmansion.com>
2024-01-02 21:47:31 +0100

    Fix bug with fine state

--- run_nerf.py
+++ run_nerf.py
@@ -807,7 +807,7 @@ def train():
             torch.save({
                 'global_step': global_step,
                 'network_fn_state_dict': render_kwargs_train['network_fn'].state_dict(),
-                'network_fine_state_dict': render_kwargs_train['network_fine'].state_dict(),
+                'network_fine_state_dict': render_kwargs_train['network_fine'].state_dict() if render_kwargs_train.get('network_fine') is not None else None,
                 'optimizer_state_dict': optimizer.state_dict(),
             }, path)
             print('Saved checkpoints at', path)
